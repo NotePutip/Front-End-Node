beb6ee5d303c35abbdd695b15a9893466b8f4317
NotePutip <s6406021620114@email.kmutnb.ac.th>
2024-02-14 10:32:15 +0700

    cc

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
-const path = requite('path')
+const path = requite("path")
 
 // const base_url = "http://localhost:3000";
 const base_url = "http://node58379-note-noderest.proen.app.ruk-com.cloud";
 
-app.set('views', path.json(__dirname, '/public/views'));
+app.set("views", path.json(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
